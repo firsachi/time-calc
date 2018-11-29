3162c02d709dd4623d7d0eedcf991f22c1b935cc
firsachi <firsachi@localhost>
2018-11-29 08:06:13 +0200

    edit id parametr

--- time-calc/src/main/java/ua/kyiv/time/calc/entities/Airline.java
+++ time-calc/src/main/java/ua/kyiv/time/calc/entities/Airline.java
@@ -6,9 +6,15 @@
 package ua.kyiv.time.calc.entities;
 
 import java.io.Serializable;
+import java.util.UUID;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 /**
  *
@@ -24,16 +30,27 @@ public class Airline implements Serializable {
 	private static final long serialVersionUID = -3503154267687059173L;
 
 	@Id
-    private int id;
+	@GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+	private UUID uuid;
     
     private String name;
+    
+    private String encoding;
+    
+    private String timeFrame;
+    
+    private String nuancesRegistration;
+    
+    private String selectionImprovedLocation;
 
-	public int getId() {
-		return id;
+	public UUID getUuid() {
+		return uuid;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public void setUuid(UUID uuid) {
+		this.uuid = uuid;
 	}
 
 	public String getName() {
@@ -43,6 +60,37 @@ public class Airline implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-    
-    
+
+	public String getEncoding() {
+		return encoding;
+	}
+
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
+
+	public String getTimeFrame() {
+		return timeFrame;
+	}
+
+	public void setTimeFrame(String timeFrame) {
+		this.timeFrame = timeFrame;
+	}
+
+	public String getNuancesRegistration() {
+		return nuancesRegistration;
+	}
+
+	public void setNuancesRegistration(String nuancesRegistration) {
+		this.nuancesRegistration = nuancesRegistration;
+	}
+
+	public String getSelectionImprovedLocation() {
+		return selectionImprovedLocation;
+	}
+
+	public void setSelectionImprovedLocation(String selectionImprovedLocation) {
+		this.selectionImprovedLocation = selectionImprovedLocation;
+	}
+
 }
