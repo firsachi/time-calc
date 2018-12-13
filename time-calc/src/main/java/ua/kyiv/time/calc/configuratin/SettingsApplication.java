507f7456a6cf8890767e86d833a95a9c80b45527
firsov <firsov@firsov.kievreclama.local>
2018-12-13 11:51:30 +0200

    delete  ObservableList<Airline> observableListAirline

--- time-calc/src/main/java/ua/kyiv/time/calc/configuratin/SettingsApplication.java
+++ time-calc/src/main/java/ua/kyiv/time/calc/configuratin/SettingsApplication.java
@@ -7,11 +7,7 @@ package ua.kyiv.time.calc.configuratin;
 
 import java.util.Locale;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import ua.kyiv.time.calc.configuratin.programfolder.FolederProgramm;
-import ua.kyiv.time.calc.dao.AirlineDao;
-import ua.kyiv.time.calc.entities.Airline;
 
 /**
  *
@@ -22,8 +18,6 @@ public class SettingsApplication {
 	public final static String DATABSE_APPLICATION_NAME = "TimeCalc.db";
 
 	public final static String USER_APLICATION_FOLDER;
-
-	private static ObservableList<Airline> observableListAirline = FXCollections.observableArrayList();
 	
 	private static Locale locale;
 
@@ -31,27 +25,10 @@ public class SettingsApplication {
 		FolederProgramm folder = new FolederProgramm();
 		USER_APLICATION_FOLDER = folder.pachProgamm();
 		locale = new Locale("uk");
-		observableListAirline.addAll(new AirlineDao().all());
 	}
 
 	public static Locale getLocale() {
 		return locale;
-	}
-
-	public static ObservableList<Airline> getObservableList() {
-		return observableListAirline;
-	}
-
-	public static ObservableList<Airline> getObservableListAirline() {
-		return observableListAirline;
-	}
-
-	public static void setObservableListAirline(ObservableList<Airline> observableListAirline) {
-		SettingsApplication.observableListAirline = observableListAirline;
-	}
-
-	public static void setObservableAirline(Airline airline) {
-		observableListAirline.add(airline);
 	}
 
 }
