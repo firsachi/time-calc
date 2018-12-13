1f3ffe3b9b4ed169837354560108841cba343013
firsov <firsov@firsov.kievreclama.local>
2018-12-13 12:17:43 +0200

    add event close form

--- time-calc/src/main/java/ua/kyiv/time/calc/MainApp.java
+++ time-calc/src/main/java/ua/kyiv/time/calc/MainApp.java
@@ -20,6 +20,7 @@ public class MainApp extends Application {
 		Scene scene = new Scene(root);
 
 		stage.setTitle(fXMLLoader.getResources().getString("key.title"));
+		stage.setOnCloseRequest(close -> System.exit(0));
 		stage.setScene(scene);
 		stage.show();
 	}
